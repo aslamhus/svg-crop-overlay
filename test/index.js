bf9ac49446f7d75eef06f7d31e62e07e6f841ba0
aslamhus <aslam.percy.husain@gmail.com>
2023-02-13 17:15:06 -0800

    initial commit

---
+++ test/index.js
@@ -0,0 +1,9 @@
+import { createCropSVG } from './svg-crop-overlay.js';
+
+const container = document.querySelector('div');
+const crop = { width: 2, height: 3 };
+const svg = { width: 640, height: 480 };
+const svgEl = createCropSVG(crop, svg);
+container.append(svgEl);
+
+// y max works for horiztonal
